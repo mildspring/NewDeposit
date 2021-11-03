db146ad46b12691551403b0311ffaebc1ce70f13
mildspring <mildspringllc@gmail.com>
2021-11-02 22:53:25 -0400

    Added a strategy to narrow down by percent.

--- LookForIpoOptimization/Form1.cs
+++ LookForIpoOptimization/Form1.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
 using System.Windows.Forms;
-using YahooFinanceApi;
 
 namespace LookForIpoOptimization
 {
@@ -42,17 +41,28 @@ namespace LookForIpoOptimization
                     _allCandles = Limit(allCandles);
                 }
 
+                // apply filters
+                var filteredCandles = Filter(_allCandles);
+
                 if (rdbMaxPercentInTimePeriod.Checked)
                 {
-                    var strategy = new MaxPercentDownAndUp(_allCandles);
-                    var result = strategy.Calculate(new TimeSpan(24 * int.Parse(txtDays.Text), 0, 0));
+                    var strategy = new MaxPercentDownAndUp(filteredCandles);
+                    var result = strategy.Calculate();
                     txtResult.AppendText($"Ticker,First.DateTime,First.High,Max.DateTime,Max.High,Min.DateTime,Min.Low");
+                    txtResult.AppendText(Environment.NewLine);
                     foreach (var oneResult in result)
                     {
-                        txtResult.AppendText(Environment.NewLine);
                         //txtResult.AppendText(oneResult.ToString());
                         txtResult.AppendText($"{oneResult.Ticker},{oneResult.First.DateTime},{oneResult.First.High},{oneResult.Max.DateTime},{oneResult.Max.High},{oneResult.Min.DateTime},{oneResult.Min.Low}");
+                        txtResult.AppendText(Environment.NewLine);
                     }
+                }
+                if (rdbIncreasedOverPercent.Checked)
+                {
+                    var strategy = new HowManyWentUpBy(filteredCandles);
+                    var result = strategy.Calculate(decimal.Parse(txtIncreasedOverPercent.Text));
+                    txtResult.AppendText(result.ToString());
+                    txtResult.AppendText(Environment.NewLine);
                 }
             }
             catch (Exception ex)
@@ -63,6 +73,30 @@ namespace LookForIpoOptimization
             {
                 this.Enabled = true;
             }
+        }
+
+        private Dictionary<string, IReadOnlyList<InternalCandle>> Filter(Dictionary<string, IReadOnlyList<InternalCandle>> allCandles)
+        {
+            // 1. limit by number of days
+            var timeSpanSinceBeginning = new TimeSpan(24 * int.Parse(txtDays.Text), 0, 0);
+            var filteredCandles = _allCandles
+                .Select(candlesOnTicker =>
+                {
+                    var first = candlesOnTicker.Value.First();
+                    var terminationDate = first.DateTime + timeSpanSinceBeginning;
+                    var candlesToConsider = candlesOnTicker.Value.TakeWhile(c => c.DateTime <= terminationDate).ToList();
+
+                    return new KeyValuePair<string, IReadOnlyList<InternalCandle>>(candlesOnTicker.Key, candlesToConsider);
+                });
+
+            // 2. limit to make sure at least this many candles exist
+            var minimumNumberOfCandles = string.IsNullOrWhiteSpace(txtMinimumNumberOfCandles.Text)
+                ? 0
+                : int.Parse(txtMinimumNumberOfCandles.Text);
+            filteredCandles = filteredCandles
+                .Where(candlesOnTicker => candlesOnTicker.Value.Count >= minimumNumberOfCandles);
+
+            return filteredCandles.ToDictionary(nvp => nvp.Key, nvp => nvp.Value);
         }
 
         private Dictionary<string, IReadOnlyList<InternalCandle>> Limit(Dictionary<string, IReadOnlyList<InternalCandle>> allCandles)
@@ -78,6 +112,27 @@ namespace LookForIpoOptimization
                 .Where(nvp => nvp.Value.First().DateTime >= date)
                 .ToDictionary(k => k.Key, v => v.Value);
             return data;
+        }
+
+        private async void btnReload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Enabled = false;
+
+                _repository.ClearCache();
+                await _repository.DeserializeAllCandles();
+                var allCandles = _repository.GetAllCandlesFromCache();
+                _allCandles = Limit(allCandles);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                this.Enabled = true;
+            }
         }
     }
 }
